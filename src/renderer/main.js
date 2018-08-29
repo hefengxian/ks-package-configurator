d5658232ec4682e51b27c3a5e41ce852fd6c30f2
hfx <jssmith883@gmail.com>
2018-08-29 20:41:20 +0800

    init project

---
+++ src/renderer/main.js
@@ -0,0 +1,19 @@
+import 'ant-design-vue/dist/antd.css'
+
+import Vue from 'vue'
+import Antd from 'ant-design-vue'
+
+import App from './App'
+import router from './router'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.config.productionTip = false
+
+Vue.use(Antd)
+
+/* eslint-disable no-new */
+new Vue({
+    components: {App},
+    router,
+    template: '<App/>'
+}).$mount('#app')
