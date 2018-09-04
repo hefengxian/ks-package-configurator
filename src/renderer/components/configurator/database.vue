d57db0ed9606472d35bde6655dee7b2c90a4cfc8
hfx <jssmith883@gmail.com>
2018-09-04 19:49:12 +0800

    according config to generate forms

--- src/renderer/components/configurator/database.vue
+++ src/renderer/components/configurator/database.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <div style="padding: 16px 0;">
+        <div class="page-header">
             <a-breadcrumb>
                 <a-breadcrumb-item>
                     <a-icon type="tool"/>
                     配置
                 </a-breadcrumb-item>
                 <a-breadcrumb-item>
-                    <a-icon type="medicine-box"/d>
+                    <a-icon type="medicine-box"/>
                     数据库配置
                 </a-breadcrumb-item>
             </a-breadcrumb>
@@ -30,34 +30,54 @@
                         配置
                     </div>
 
-                    <div class="config-form">d
+                    <div class="config-form">
                         <a-card title="数据库配置修改">
                             <!--<a href="#" slot="extra">more</a>-->
                             <a-form>
-                                <a-form-item label="原 Data 目录"
+                                <a-form-item v-for="(item, key) in configForm.items"
+                                             :key="key"
                                              :labelCol="{ span: 5 }"
                                              :wrapperCol="{ span: 12 }">
-                                    <a-input-search >
+                                    <!-- 表单标题 -->
+                                    <span slot="label">{{item.label}}
+                                        <a-tooltip v-if="item.helpText && item.helpText !== ''"
+                                                   :title="item.helpText">
+                                            <a-icon type='question-circle-o' />
+                                        </a-tooltip>
+                                    </span>
+
+                                    <!-- 表单为路径的时候 -->
+                                    <a-input-search v-if="item.type === 'path'"
+                                                    v-model.trim="item.value">
                                         <a-button icon="folder-open"
                                                   title="打开目录"
                                                   slot="enterButton"></a-button>
                                     </a-input-search>
+
+                                    <!-- 表达类型为数字的时候 -->
+                                    <a-input-number v-else="item.type === 'number'"
+                                                    v-model="item.value" />
+
+                                    <!-- 其他类型，默认为字符串，普通表单 -->
+                                    <a-input v-else
+                                             v-model.trim="item.value" />
                                 </a-form-item>
-                                <a-form-item label="新 Data 目录"
-                                             :labelCol="{ span: 5 }"
+
+                                <!-- 是否拷贝目录 -->
+                                <a-form-item :labelCol="{ span: 5 }"
                                              :wrapperCol="{ span: 12 }">
-                                    <a-input-search>
-                                        <a-button icon="folder-open"
-                                                  title="打开目录"
-                                                  slot="enterButton"></a-button>
-                                    </a-input-search>
+                                    <span slot="label">拷贝目录
+                                        <a-tooltip>
+                                            <span slot="title">是否将原 Data 目录下的文件复制到新的 Data 目录</span>
+                                            <a-icon type='question-circle-o' />
+                                        </a-tooltip>
+                                    </span>
+                                    <a-switch>
+                                        <a-icon type="check" slot="checkedChildren"/>
+                                        <a-icon type="cross" slot="unCheckedChildren"/>
+                                    </a-switch>
                                 </a-form-item>
-                                <a-form-item label="Innodb pool size"
-                                             :labelCol="{ span: 5 }"
-                                             :wrapperCol="{ span: 12 }">
-                                    <a-input-number :min="1"/>
-                                    GB
-                                </a-form-item>
+
                                 <a-form-item class="form-submit"
                                              :wrapperCol="{ span: 12, offset: 5 }">
                                     <a-button>预览</a-button>
@@ -74,20 +94,40 @@
 
 <script>
     import {remote, shell} from 'electron'
+    import {Form} from 'ant-design-vue'
 
     export default {
         name: "common-configurator",
         data() {
             return {
                 currentTab: 'doc',
-
-
-                configForms: [
-                    {
-                        title: '',
-
-                    }
-                ],
+                configForm: {
+                    title: '',
+                    items: [
+                        {
+                            type: 'path',
+                            label: '原 Data 目录',
+                            name: 'oldPath',
+                            value: 'D:\\KWM\\Database_Server\\Data',
+                            placeholder: '',
+                        },
+                        {
+                            type: 'path',
+                            label: '新 Data 目录',
+                            name: 'newPath',
+                            value: 'E:\\Data',
+                            placeholder: '',
+                        },
+                        {
+                            type: 'number',
+                            label: 'Innodb pool size',
+                            name: 'poolSize',
+                            value: 1,
+                            placeholder: '',
+                            helpText: '单位 GB，如果是单独的数据库服务器，应该配置为机器物理内存的 70% - 80%',
+                        },
+                    ],
+                }
             }
         },
         methods: {
